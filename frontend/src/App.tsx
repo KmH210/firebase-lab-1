4922fd530a16c7644b5832b41dedd8cc9a4340e5
katie <kholland210@gmail.com>
2021-06-08 09:57:41 -0400

    updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import ShoutOutList from './components/ShoutOutList';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import ShoutOutCard from './components/ShoutOutCard';
 import ShoutOutTo from './components/ShoutOutTo';
 
@@ -13,9 +13,9 @@ function App() {
           <Route path="/" exact>
             <ShoutOutList/>
           </Route>
-          <Route path="/shoutoutto/:to">
+          {/* <Route path="/shoutoutto/:to"> */}
             <ShoutOutTo/>
-          </Route>
+          {/* </Route> */}
         </Switch>
       </div>
     </Router>
